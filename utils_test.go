39c008749f4f8d4d65a3422d6b4a705fda8f2b74
Tryanks <tryanks@outlook.com>
2024-03-29 14:55:28 +0800

    Improve test

--- utils_test.go
+++ utils_test.go
@@ -5,16 +5,7 @@ import (
 )
 
 func TestGetProtocol(t *testing.T) {
-	devices, err := getDevices(1, true)
-	if err != nil {
-		t.Error(err)
-	}
-	for _, v := range devices {
-		t.Log(v.Protocol)
-		_ = v.Close()
-	}
-
-	devices, err = GetDevices(1)
+	devices, err := GetDevices(1)
 	if err != nil {
 		t.Error(err)
 	}
